5426442856309d1b6095d3e02996cbd4002a0681
karlovsek <43514900+karlovsek@users.noreply.github.com>
2023-05-17 12:56:45 +0200

    Update install-minimal.sh
    Fix lazygit installation

--- install-minimal.sh
+++ install-minimal.sh
@@ -29,7 +29,8 @@ else
   echo "Installing lazygit"
   wget https://github.com/jesseduffield/lazygit/releases/download/v0.37.0/lazygit_0.37.0_Linux_x86_64.tar.gz
   tar -xf lazygit_0.37.0_Linux_x86_64.tar.gz
-  mv lazygit ~/.local/bin
+  mkdir -p ~/.local/bin
+  mv lazygit ~/.local/bin/
 fi
 
 
