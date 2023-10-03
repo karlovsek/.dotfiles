d617d53ef77a427228bc30498fd9016fb6d6270e
karlovsek <43514900+karlovsek@users.noreply.github.com>
2023-10-03 10:37:10 +0200

    Update install-minimal.sh
    Fix installation, and automatic symlink generation
    
    Update install-minimal.sh
    
    Small fixes
    
    Update install-minimal.sh
    
    Default Y
    Symlinks for zsh must be created at the end
    
    Update install-minimal.sh
    
    fix if
    
    Update install-minimal.sh
    
    echo fix
    
    Update install-minimal.sh
    
    echo fix
    
    Update install-minimal.sh
    
    Install fasd

--- install-minimal.sh
+++ install-minimal.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "SCRIPT_DIR=${SCRIPT_DIR}"
 
 if which zsh >/dev/null; then
     echo "ZSH exists"
@@ -7,22 +9,21 @@ else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)"
 fi
 
-# install oh my zsh
-echo "Insalling oh my zsh and plugins ..."
-echo "After installation press Ctrl-C, to exit ZSH and to continue the installation. Now press ENTER to continue!"
-read foo
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 if which fzf >/dev/null; then
     echo "fzf exists"
 else
   echo "Installing fzf"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
+fi
+
+if which fasd >/dev/null; then
+    echo "fasd exists"
+else
+  echo "Installing fasd"
+  wget https://github.com/clvv/fasd/zipball/1.0.1 -O fasd.zip
+  unzip -p fasd.zip clvv-fasd-4822024/fasd > ~/.local/bin/fasd
+  chmod +x ~/.local/bin/fasd
 fi
 
 if which lazygit >/dev/null; then
@@ -35,9 +36,50 @@ else
   mv lazygit ~/.local/bin/
 fi
 
+echo -ne "\nCreate Vim symlinks? (Y/n): "
+read answer
+answer=$(tr "[A-Z]" "[a-z]" <<< "$answer") 
+if [[ "$answer" == "y" || -z "$answer" ]]; then
+    if [ -f ~/.vimrc ]; then
+      mv ~/.vimrc ~/.vimrc_orig
+    fi
+    if [ -f ~/.vimcommon ]; then
+      mv ~/.vimcommon ~/.vimcommon_orig
+    fi
+    ln -s ${SCRIPT_DIR}/vim/.vimrc ~/.vimrc
+    ln -s ${SCRIPT_DIR}/vim/.vimcommon ~/.vimcommon
+    echo -e "\tSymlinks created!"
+else
+  echo "You can create Vim symlinks as:"
+  echo "ln -s ${SCRIPT_DIR}/vim/.vimrc ~/.vimrc && ln -s ${SCRIPT_DIR}/vim/.vimcommon ~/.vimcommo"
+fi
+
+# install oh my ZSH
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install oh my ZSH plugins, must be after installing oh-my-zsh
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo -ne "\nCreate zshrc and p10k symlinks? (Y/n): "
+read answer
+answer=$(tr "[A-Z]" "[a-z]" <<< "$answer") 
+if [[ "$answer" == "y" || -z "$answer" ]]; then
+    if [ -f ~/.zshrc ]; then
+      mv ~/.zshrc ~/.zshrc_orig
+    fi
+    if [ -f ~/..p10k.zsh ]; then
+      mv ~/.p10k.zsh ~/.p10k.zsh_orig
+    fi
+    ln -s ${SCRIPT_DIR}/zsh/.zshrc ~/.zshrc
+    ln -s ${SCRIPT_DIR}/zsh/.p10k.zsh ~/.p10k.zsh
+    echo -e "\tSymlinks created!"
+else
+  echo "You can create ZSH symlinks as:"
+  echo "ln -s ${SCRIPT_DIR}/zsh/.zshrc ~/.zshrc &&  ln -s ${SCRIPT_DIR}/zsh/.p10k.zsh ~/.p10k.zsh"
+fi
+
 echo "Installation completed!"
-echo "add symlinks:"
-echo "ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc"
-echo "ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh"
-echo  ""
-echo "Now you can run ZSH!"
+zsh # run ZSH
