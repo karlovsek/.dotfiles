23bebc17e116e71ae8ee184a1706d65101556572
karlovsek <43514900+karlovsek@users.noreply.github.com>
2023-10-04 13:19:59 +0200

    RUNZSH=no

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # install nix
-echo "Do you want to install TUIs? [y/N]"
+echo "Do you want to install Nix TUIs? [y/N]"
 read response
 # Check the value of the response variable and proceed accordingly
 if [ "$response" = "y" ]; then
@@ -9,7 +9,6 @@ if [ "$response" = "y" ]; then
   chmod +x install.sh && ./install.sh
   cd ..
 fi
-
 
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
@@ -23,12 +22,11 @@ if [ "$response" = "y" ]; then
 fi
 
 echo "Installing ZSH plugins..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 
 # stow dotfiles
 #stow vim
