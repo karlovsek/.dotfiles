8bd6beb134a62dc705c230ea1caf9f004a31296e
Peter Karlovsek <peter.karlovsek@gmail.com>
2023-10-20 14:40:53 +0200

    nvim terminal shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd("set runtimepath^=~/.vim runtimepath+=~/.vim/after")
+vim.o.packpath = vim.o.runtimepath
+vim.cmd("source ~/.vimcommon")
