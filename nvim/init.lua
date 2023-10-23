93c77c496842a9b51736c5f9f516c0ae6cc005a0
karlovsek <43514900+karlovsek@users.noreply.github.com>
2023-10-23 09:10:33 +0200

    Telescope follow

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,24 @@ require("config.lazy")
 vim.cmd("set runtimepath^=~/.vim runtimepath+=~/.vim/after")
 vim.o.packpath = vim.o.runtimepath
 vim.cmd("source ~/.vimcommon")
+
+require("telescope").setup({
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--follow",
+    },
+  },
+  pickers = {
+    find_files = {
+      follow = true,
+    },
+  },
+})
+
