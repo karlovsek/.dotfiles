aa029aa8df59f933e805bd8044448afd4c327617
karlovsek <43514900+karlovsek@users.noreply.github.com>
2023-10-31 19:14:17 +0100

    Bump layzgit

--- install-minimal.sh
+++ install-minimal.sh
@@ -70,7 +70,7 @@ if which lazygit >/dev/null; then
 	echo "lazygit exists"
 else
 	echo "Installing lazygit"
-	wget https://github.com/jesseduffield/lazygit/releases/download/v0.37.0/lazygit_0.37.0_Linux_x86_64.tar.gz
+	wget https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz
 	tar -xf lazygit_0.37.0_Linux_x86_64.tar.gz
 	mkdir -p ~/.local/bin
 	mv lazygit ~/.local/bin/
