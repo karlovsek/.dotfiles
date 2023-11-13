e116d28f7138469de6e50808c3e249c87fe7aaf3
karlovsek <43514900+karlovsek@users.noreply.github.com>
2023-11-13 09:54:09 +0100

    Adds detour nvim plugin

---
+++ nvim/lua/plugins/detour.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "carbon-steel/detour.nvim",
+    config = function()
+      vim.keymap.set("n", "<c-w><enter>", ":Detour<cr>")
+    end,
+  },
+  -- A keymap for selecting a terminal buffer to open in a popup
+  vim.keymap.set("n", "<leader>t", function()
+    require("detour").Detour() -- Open a detour popup
+
+    -- Switch to a blank buffer to prevent any accidental changes.
+    vim.cmd.enew()
+    vim.bo.bufhidden = "delete"
+
+    require("telescope.builtin").buffers({}) -- Open telescope prompt
+    vim.api.nvim_feedkeys("term", "n", true) -- popuplate prompt with "term"
+  end),
+}
