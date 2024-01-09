1b2193f338a66af14442e7e274888210e6922767
karlovsek <43514900+karlovsek@users.noreply.github.com>
2024-01-09 12:00:30 +0100

    Nvim osc52

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 -- Add any additional keymaps here
 --
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
+
+vim.keymap.set('n', '<leader>o', require('osc52').copy_operator, {expr = true})
+vim.keymap.set('n', '<leader>oo', '<leader>c_', {remap = true})
+vim.keymap.set('v', '<leader>o', require('osc52').copy_visual)
