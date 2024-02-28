32664033c2fba8416d3126fd2e1a4f685bd14af7
karlovsek <peter.karlovsek@gmail.com>
2024-02-28 13:16:15 +0100

    vimcommin fixes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
+-- vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 
-vim.keymap.set('n', '<leader>o', require('osc52').copy_operator, {expr = true})
-vim.keymap.set('n', '<leader>oo', '<leader>c_', {remap = true})
-vim.keymap.set('v', '<leader>o', require('osc52').copy_visual)
+vim.keymap.set("n", "<leader>o", require("osc52").copy_operator, { expr = true })
+vim.keymap.set("n", "<leader>oo", "<leader>c_", { remap = true })
+vim.keymap.set("v", "<leader>o", require("osc52").copy_visual)
+
