5ca823e93359c4ba58b8554db8efa49018eca7a7
karlovsek <peter.karlovsek@gmail.com>
2024-03-06 06:56:51 +0100

    Vim catppuccini

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd.colorscheme("tokyonight-night")
+vim.cmd.colorscheme("catppuccin")
 
 vim.cmd("set runtimepath^=~/.vim runtimepath+=~/.vim/after")
 vim.o.packpath = vim.o.runtimepath
