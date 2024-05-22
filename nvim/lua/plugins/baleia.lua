1c5e1746a6f659225a78f8d3558eb6b7ee39bfdd
karlovsek <peter.karlovsek@gmail.com>
2024-05-22 10:09:11 +0200

    Fix nvim baleia

--- nvim/lua/plugins/baleia.lua
+++ nvim/lua/plugins/baleia.lua
@@ -1,5 +1,14 @@
 return {
   {
-    { "m00qek/baleia.nvim" },
+    {
+      "m00qek/baleia.nvim",
+      config = function()
+        vim.g.conjure_baleia = require("baleia").setup({ line_starts_at = 3 })
+
+        vim.api.nvim_create_user_command("BaleiaColorize", function()
+          vim.g.conjure_baleia.once(vim.api.nvim_get_current_buf())
+        end, { bang = true })
+      end,
+    },
   },
 }
