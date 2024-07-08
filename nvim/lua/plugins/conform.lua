60a8eaa4d6e69e8e67ab802723fe44c77a3c88fe
karlovsek <43514900+karlovsek@users.noreply.github.com>
2024-07-08 13:13:33 +0200

    Fix shfmt args

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     formatters = {
       shfmt = {
-        extra_args = { "-i", "2", "-ci", "-bn" },
+        prepend_args = { "-i", "2", "-ci", "-bn" },
       },
     },
   },
