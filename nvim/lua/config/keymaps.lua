cb0a38ee3185164627388d2743eb565ae494ffa5
karlovsek <43514900+karlovsek@users.noreply.github.com>
2024-10-10 22:35:58 +0200

    nvim add better-escape

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
--- vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
@@ -13,6 +12,13 @@ keymap.set("n", "*", "*zz", opts)
 keymap.set("n", "#", "#zz", opts)
 keymap.set("n", "g*", "g*zz", opts)
 keymap.set("n", "g#", "g#zz", opts)
+
+keymap.set("t", ",,", "<C-\\><C-n>", opts)
+keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", opts)
+keymap.set("t", "<Esc>h", "<<C-\\><C-n>C-w>h<CR>", opts)
+keymap.set("t", "<Esc>j", "C-\\><C-n><C-w>j<CR>", opts)
+keymap.set("t", "<Esc>k", "-\\><C-n><C-w>k<CR>", opts)
+keymap.set("t", "<Esc>l", "C-\\><C-n><C-w>l<CR>", opts)
 
 vim.keymap.set("n", "<leader>o", require("osc52").copy_operator, { expr = true })
 vim.keymap.set("n", "<leader>oo", "<leader>c_", { remap = true })
