174904adbc3402d51c398dcf2b9c21d488dc7977
karlovsek <43514900+karlovsek@users.noreply.github.com>
2024-10-10 22:46:52 +0200

    nvim fix typo 2

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,6 +6,8 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+keymap.set("n", "VA", "ggVG", opts)
+
 keymap.set("n", "n", "nzz", opts)
 keymap.set("n", "N", "Nzz", opts)
 keymap.set("n", "*", "*zz", opts)
@@ -15,10 +17,10 @@ keymap.set("n", "g#", "g#zz", opts)
 
 keymap.set("t", ",,", "<C-\\><C-n>", opts)
 keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", opts)
-keymap.set("t", "<Esc>h", "<<C-\\><C-n>C-w>h<CR>", opts)
-keymap.set("t", "<Esc>j", "C-\\><C-n><C-w>j<CR>", opts)
-keymap.set("t", "<Esc>k", "-\\><C-n><C-w>k<CR>", opts)
-keymap.set("t", "<Esc>l", "C-\\><C-n><C-w>l<CR>", opts)
+keymap.set("t", "<Esc>h", "<C-\\><C-n>C-w>h<CR>", opts)
+keymap.set("t", "<Esc>j", "<C-\\><C-n><C-w>j<CR>", opts)
+keymap.set("t", "<Esc>k", "<C-\\><C-n><C-w>k<CR>", opts)
+keymap.set("t", "<Esc>l", "<C-\\><C-n><C-w>l<CR>", opts)
 
 vim.keymap.set("n", "<leader>o", require("osc52").copy_operator, { expr = true })
 vim.keymap.set("n", "<leader>oo", "<leader>c_", { remap = true })
