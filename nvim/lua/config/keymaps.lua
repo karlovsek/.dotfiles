a0a9cc6ab1a27cb7696b211e9d08a14a210e074e
karlovsek <karlosp@gmail.com>
2024-10-23 06:37:36 +0200

    nvim fix osc52 keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,12 +16,7 @@ keymap.set("n", "g*", "g*zz", opts)
 keymap.set("n", "g#", "g#zz", opts)
 
 keymap.set("t", "jj", "<cmd>ToggleTerm<CR>", opts)
--- keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", opts)
--- keymap.set("t", "<Esc>h", "<C-\\><C-n>C-w>h<CR>", opts)
--- keymap.set("t", "<Esc>j", "<C-\\><C-n><C-w>j<CR>", opts)
--- keymap.set("t", "<Esc>k", "<C-\\><C-n><C-w>k<CR>", opts)
--- keymap.set("t", "<Esc>l", "<C-\\><C-n><C-w>l<CR>", opts)
 
 vim.keymap.set("n", "<leader>o", require("osc52").copy_operator, { expr = true })
-vim.keymap.set("n", "<leader>oo", "<leader>c_", { remap = true })
+vim.keymap.set("n", "<leader>oo", "<leader>o_", { remap = true })
 vim.keymap.set("v", "<leader>o", require("osc52").copy_visual)
