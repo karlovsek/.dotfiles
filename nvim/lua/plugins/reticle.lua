5d847136f84d62463f5c475decf36e5202e45d06
karlovsek <43514900+karlovsek@users.noreply.github.com>
2024-11-08 09:13:58 +0100

    nvim add reticle plugin

---
+++ nvim/lua/plugins/reticle.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "tummetott/reticle.nvim",
+    event = "VeryLazy", -- optionally lazy load the plugin
+    opts = {
+      -- add options here if you wish to override the default settings
+      -- Define filetypes which are ignored by the plugin
+      ignore = {
+        cursorline = {
+          "DressingInput",
+          "FTerm",
+          "NvimSeparator",
+          "NvimTree",
+          "TelescopePrompt",
+          "Trouble",
+          "dashboard",
+        },
+        cursorcolumn = {},
+      },
+    },
+  },
+}
