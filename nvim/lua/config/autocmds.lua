c252601dc1bfc14ac903c37f719b64c02b8caa29
Peter Karlovsek <peter.karlovsek@gmail.com>
2025-02-01 20:00:40 +0100

    nvim fix c++ commenting

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,3 +3,12 @@
 -- Add any additional autocmds here
 
 vim.cmd([[autocmd TermOpen,TermEnter,BufWinEnter,WinEnter term://* startinsert]])
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  callback = function()
+    local ft = vim.bo.filetype
+    if ft == "cpp" or ft == "c" or ft == "hpp" then -- Check for multiple filetypes
+      vim.bo.commentstring = "// %s"
+    end
+  end,
+})
