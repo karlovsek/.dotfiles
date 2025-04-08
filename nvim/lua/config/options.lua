d31698f92c677ea1681aa63f338f2fc134e1a459
karlovsek <43514900+karlovsek@users.noreply.github.com>
2025-04-08 08:41:00 +0200

    nvim set OSC 52 as default

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -23,3 +23,24 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = "*",
   command = "setlocal norelativenumber",
 })
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
